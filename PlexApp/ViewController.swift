d6360f39fe6a97af6458a4ce861b697e77bbb101
shavinimesh <39796963+shavinimesh@users.noreply.github.com>
2021-01-24 11:26:40 +0530

    Add files via upload

---
+++ PlexApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PlexApp
+//
+//  Created by Hishara Dilshan on 2021-01-24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
